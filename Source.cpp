44258b4015b4677cd279f7da7c98c4ad3344b1f9
dryErase <beno695@gmail.com>
2016-10-28 14:39:12 -0700

    New commit
    This is the first commit

---
+++ Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+	string input = "", 
+		   reverse = "";
+
+	cout << "Enter a string: ";
+	cin >> input;
+
+	transform(input.begin(), input.end(), input.begin(), ::tolower);
+
+	for (int i = input.length()-1; i >= 0; --i)
+		reverse += input[i];
+
+	cout << reverse << endl;;
+
+	return 0;
+}
